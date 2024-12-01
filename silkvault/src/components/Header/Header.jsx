175acf7aaa216ad6fa92fc4136ae032d44836c1d
SylvanasBV <andres7.7mac@gmail.com>
2024-12-01 10:44:15 -0500

    Navbar start develop

---
+++ silkvault/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+import React from 'react'
+
+function header() {
+  return (
+    <div>
+      <div>Hola mundo</div>
+    </div>
+  )
+}
+
+export default header
--- silkvault/src/index.jsx
+++ silkvault/src/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.jsx';
 
+import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+
+import "@fontsource/outfit";
+import "@fontsource/roboto";
+
+const router = createBrowserRouter([
+  { path: "/TheSilkVault.github.io/", element: <App /> }, // Renderiza App
+  { path: "/TheSilkVault.github.io/about", element: <h1>About</h1> }, // Ruta secundaria
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
@@ -15,3 +24,17 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+/*
+import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+reportWebVitals();
+*/
