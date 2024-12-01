c31471668383902d4802289273de9ad0c8c93d68
SylvanasBV <andres7.7mac@gmail.com>
2024-12-01 18:57:46 -0500

    First Part Own Me

--- silkvault/src/App.jsx
+++ silkvault/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './App.module.css';
 import Navbar from './components/NavBar/NavBar';
+import Head_Page from './components/Head_Page/Head_Page';
 function App() {
   return (
     <div className="App">
       <div className={styles.App}>
         <Navbar/>
+        <Head_Page/>
       </div>
     </div>
   );
---
+++ silkvault/src/components/Head_Page/Head_Page.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { getImageUrl } from '../../utils';
+import styles from "./Head_Page.module.css";
+const Head_Page = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}> Hi, I'm Andres </h1>
+        <p className={styles.description}> 
+            I'm a full-stack-developer with 1 year of experience using React and NodeJS.
+            with knowledge in Java,Python,C#.
+            <h5>Welcome to my voult</h5>
+        </p>
+        
+        <a href="mailto:andres7.7mac@gmail.com" 
+        className={styles.contactBtn}>
+            Contact Me
+        </a>
+      </div>
+      <img src={getImageUrl("character/heroImage.png")} alt="Character image of me" className={styles.heroImg}/>
+       <div className={styles.topBlur} />
+       <div className={styles.bottomBlur}/>
+    </section>
+  )
+}
+
+export default Head_Page
